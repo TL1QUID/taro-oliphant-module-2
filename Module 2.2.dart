4c31b2eeb91a7cdeb716f16d60e33029cf42dab8
TL1QUID <104706358+TL1QUID@users.noreply.github.com>
2022-05-31 09:48:56 +0200

    Add files via upload

---
+++ Module 2.2.dart
@@ -0,0 +1,8 @@
+
+var arr = ['FNB banking app','Snapscan','LiveInspect','NumDrop', 'Domestly','Shyft', 'Khula ecosystem', 'Naked insurance','EasyEquitiies','Edtech'];
+var arr1 =['2012','2013','2014','2015','2016','2017','2018','2019','2020','2021'];
+void main(){
+  print(arr);
+  print(arr.length);
+  print(arr[5]);
+  print(arr[6]);}
